c0550a3d4cf35b3eabb4683b515ac693d2974be6
Zi Ye <18612319597@163.com>
2021-01-03 18:19:16 +0800

    feat: collect and uncollect

--- pages/post-details/post-details.js
+++ pages/post-details/post-details.js
@@ -3,23 +3,45 @@ import {
   postList
 } from '../../data/data.js';
 
+const COLLECTED_POSTS = 'COLLECTED_POSTS';
+
 Page({
 
   /**
    * Page initial data
    */
   data: {
-    postDetails: {}
+    postDetails: {},
+    collected: false,
+    _pid: null,
+    _collectedPosts: []
+  },
+
+  onCollect() {
+    if (this.data.collected) {
+      this.data._collectedPosts = this.data._collectedPosts.filter(item => item !== this.data._pid);
+    } else {
+      this.data._collectedPosts = [...this.data._collectedPosts, this.data._pid];
+    }
+    this.setData({
+      collected: !this.data.collected
+    });
+    wx.setStorageSync(COLLECTED_POSTS, this.data._collectedPosts);
+    wx.showToast({
+      title: this.data.collected ? '收藏成功' : '取消收藏'
+    })
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-    const pid = parseInt(options.pid);
-    const postDetails = postList.find(item => item.postId === pid);
+    this.data._pid = parseInt(options.pid);
+    this.data._collectedPosts = wx.getStorageSync(COLLECTED_POSTS) || [];
+    const postDetails = postList.find(item => item.postId === this.data._pid);
     this.setData({
-      postDetails
+      postDetails,
+      collected: this.data._collectedPosts.includes(this.data._pid)
     });
   },
 
