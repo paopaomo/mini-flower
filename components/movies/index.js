e6d94e88d964c6c8c29472d1a3292d0b63bce6d1
Zi Ye <18612319597@163.com>
2021-01-12 22:15:46 +0800

    feat: movies and movie component

---
+++ components/movies/index.js
@@ -0,0 +1,23 @@
+// components/movies/index.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {},
+
+  externalClasses: ['f-class'],
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
