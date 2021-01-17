f7a9973d3e42b58e06992be9f1f2b1b0a8a901d7
Zi Ye <zi.ye1@thoughtworks.com>
2021-01-17 16:19:29 +0800

    feat: set navigation bar title text by 2 ways

--- pages/more-movie/more-movie.js
+++ pages/more-movie/more-movie.js
@@ -55,7 +55,24 @@ Page({
    * Lifecycle function--Called when page is initially rendered
    */
   onReady: function () {
-
+    let title = '电影'
+    switch(this.data._type) {
+      case 'in_theaters':
+        title = '正在热映'
+        break
+      case 'coming_soon':
+        title = '即将上映'
+        break
+      case 'top250':
+        title = '豆瓣Top250'
+        break
+      default:
+        title = '正在热映'
+        break   
+    }
+    wx.setNavigationBarTitle({
+      title,
+    })
   },
 
   /**
